97025f2d44093c9d34da7f548ccbbfc18a992498
Al Skipp <al_skipp@fastmail.fm>
2020-08-10 23:18:05 +0100

    Replace closures with keypaths in CaseStudies examples

--- Examples/CaseStudies/SwiftUICaseStudies/02-Effects-Cancellation.swift
+++ Examples/CaseStudies/SwiftUICaseStudies/02-Effects-Cancellation.swift
@@ -91,7 +91,7 @@ struct EffectsCancellationView: View {
         ) {
           Stepper(
             value: viewStore.binding(
-              get: { $0.count }, send: EffectsCancellationAction.stepperChanged)
+              get: \.count, send: EffectsCancellationAction.stepperChanged)
           ) {
             Text("\(viewStore.count)")
           }
